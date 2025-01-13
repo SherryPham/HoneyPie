f89a92c8bc53a81ccf0b7526b6dc02ea25744ad6
Sherry <phamthuthu204@gmail.com>
2025-01-13 12:33:58 +1100

    emulated shell

--- ssh_honeypot.py
+++ ssh_honeypot.py
@@ -1,5 +1,6 @@
 import logging
 from logging.handlers import RotatingFileHandler
+import socket
 
 # Constant
 logging_format = logging.Formatter('%(message)s')
@@ -11,10 +12,12 @@ funnel_handler = RotatingFileHandler('audits.log', maxBytes=2000, backupCount=5)
 funnel_handler.setFormatter(logging_format)
 funnel_logger.addHandler(funnel_handler)
 
-creds_logger = logging.getLogger('FunnelLogger')
+creds_logger = logging.getLogger('CredsLogger')
 creds_logger.setLevel(logging.INFO)
 creds_handler = RotatingFileHandler('cmd_audits.log', maxBytes=2000, backupCount=5)
 creds_handler.setFormatter(logging_format)
 creds_logger.addHandler(creds_handler)
 
 # Emulated Shell
+def emulated_shell(channel, client_ip):
+    channel.send(b'corporate-jumpbox2$')
